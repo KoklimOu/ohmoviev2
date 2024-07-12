c0c96b993b5dd4e8656eef69161cc4c037e31aa6
KoklimOu <oukoklim@gmail.com>
2024-07-12 23:09:59 +0700

    Replace fetch to axios, and change tmdb apikey to access token.

--- src/sections/movie-section/MovieSection.jsx
+++ src/sections/movie-section/MovieSection.jsx
@@ -21,13 +21,61 @@ function MovieSection() {
     }, []);
     
     return (
-        <div>
-            <ul>
-            {popMovies.map(movie => (
-                <VidSrcCard key={movie.id} movie={movie}/>
-            ))}
-            </ul>
-        </div>
+        <>
+            <div className={styles.movieContainer}>
+                {popMovies.length > 0 && (
+                    <div className={styles.firstItem} onClick={() => onclickCard(popMovies[0].id, popMovies[0].media_type)}>
+                        <img src={`https://image.tmdb.org/t/p/original${popMovies[0].backdrop_path}`} alt={popMovies[0].title} />
+                        <div className={styles.overlayText}>
+                            <h1>{popMovies[0].title}</h1>
+                            <p>{popMovies[0].overview}</p>
+                        </div>
+                    </div>
+                )}
+                
+                <h2>Popular Movies</h2>
+
+                <ul className={styles.moviesGrid}>
+                    {popMovies.slice(1).map(movie => (
+                        <img
+                            key={movie.id}
+                            onClick={() => onclickCard(movie.id, movie.media_type)}
+                            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                            alt={movie.title}
+                        />
+                    ))}
+                </ul>
+
+            </div>
+
+
+            <div className={styles.movieContainer}>
+                {trendings.length > 0 && (
+                    <div className={styles.firstItem} onClick={() => onclickCard(trendings[0].id, trendings[0].media_type)}>
+                        <img src={`https://image.tmdb.org/t/p/original${trendings[0].backdrop_path}`} alt={trendings[0].title} />
+                        <div className={styles.overlayText}>
+                            <h1>{trendings[0].title}</h1>
+                            <p>{trendings[0].overview}</p>
+                        </div>
+                    </div>
+                )}
+                
+                <h2>Trending Now</h2>
+
+                <ul className={styles.moviesGrid}>
+                    {trendings.slice(1).map(trending => (
+                        <img
+                            key={trending.id}
+                            onClick={() => onclickCard(trending.id, trending.media_type)}
+                            src={`https://image.tmdb.org/t/p/original${trending.backdrop_path}`}
+                            alt={trending.title}
+                        />
+                    ))}
+                </ul>
+
+            </div>
+        </>
+
     );
 }
 
--- src/services/themoviedbApi.js
+++ src/services/themoviedbApi.js
@@ -1,13 +1,46 @@
-const TMDB_API_KEY = `${import.meta.env.VITE_TMDB_API_KEY}`;
+import axios from 'axios';
+
+const TMDB_ACCESS_TOKEN = `${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`;
+
+const axiosInstance = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${TMDB_ACCESS_TOKEN}`,
+  },
+});
 
 export const fetchPopularMovies = async () => {
   try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${TMDB_API_KEY}`);
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    const data = await response.json();
-    return data.results;
+    const response = await axiosInstance.get('/movie/popular');
+    return response.data.results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchTrendings = async () => {
+  try {
+    const response = await axiosInstance.get('/trending/all/day');
+    return response.data.results;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchMovieDetails = async (tmdbId) => {
+  try {
+    const response = await axiosInstance.get(`/movie/${tmdbId}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchSerieDetails = async (tmdbId) => {
+  try {
+    const response = await axiosInstance.get(`/tv/${tmdbId}`);
+    return response.data;
   } catch (error) {
     throw error;
   }
